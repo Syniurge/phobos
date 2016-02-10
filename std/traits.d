154bc6adf6974d9c4abd0cc595241ad3eeaf19a3
Elie Morisse <syniurge@gmail.com>
2016-02-10 12:54:59 -0300

    Do not assume that every class derives from Object in BaseClassesTuple!T.
    (cherry picked from commit 5a1ae05e1cf66aadc8c297559b699867fddcf9a8)

--- std/traits.d
+++ std/traits.d
@@ -4042,7 +4042,7 @@ template BaseTypeTuple(A)
 template BaseClassesTuple(T)
     if (is(T == class))
 {
-    static if (is(T == Object))
+    static if (is(T == Object) || !BaseTypeTuple!T.length) // CALYPSO
     {
         alias BaseClassesTuple = AliasSeq!();
     }
